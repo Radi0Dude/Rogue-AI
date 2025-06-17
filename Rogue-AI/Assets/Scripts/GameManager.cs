6b9a793328c6a84c936f1c8385b9075d06f784db
Takotzo <oyvindroberg@hotmail.no>
2025-06-17 15:30:32 +0200

    GameManager fixes scene Swaps

--- Rogue-AI/Assets/Scripts/GameManager.cs
+++ Rogue-AI/Assets/Scripts/GameManager.cs
@@ -1,5 +1,5 @@
-using TMPro;
-using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public static class GameManager
 {
@@ -10,6 +10,28 @@ public static class GameManager
     
     // Player Variables
     public static int PlayerMaxHealth = 100;
+
+    private static List<RoomData> _rooms = new ();
+
+    public static void AddToRoomList(List<RoomData> addedRooms)
+    {
+        foreach (var newRoom in addedRooms)
+        {
+            _rooms.Add(newRoom);
+        }
+    }
+
+    private static void LoadNextRoom()
+    {
+        SceneManager.LoadScene(_rooms[0].sceneNameToLoad);
+    }
+
+    public static void RemoveCompletedRoom()
+    {
+        _rooms.RemoveAt(0);
+
+        LoadNextRoom();
+    }
     
     
 }
