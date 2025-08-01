828a3cb57ba741b4bcf13f7a94c9cd931a91da03
Radi0Dude <tobias.fehn@hotmail.com>
2025-08-01 14:29:13 +0200

    Update PromptManager.cs

--- Rogue-AI/Assets/Scripts/PromptSystem/Testing new System/PromptManager.cs
+++ Rogue-AI/Assets/Scripts/PromptSystem/Testing new System/PromptManager.cs
@@ -1,11 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PromptManager : MonoBehaviour
 {
-    
 
-    public void GetTemplate()
-    {
+	string currentPromt;
 
-    }
+	List<Prompts> promptData = new List<Prompts>();
+
+	public void SetPrompt(string prompt)
+	{
+		currentPromt = prompt;
+		Debug.Log("Current Prompt Set: " + currentPromt);
+	}
+
+	public void GetStartPrompt()
+	{
+		int RandomIndex = Random.Range(0, promptData.Count);
+
+		SetPrompt(promptData[RandomIndex].promptText);
+	}
+
+	public void UpdatePromptWithCard(string promptAddition, string placement, string[] newtags)
+	{
+
+	}
 }
